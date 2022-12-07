181c94a4be331c03be273da6ff482f2194d7f2b6
Michael Stratton <mastratton3@gmail.com>
2022-12-07 10:20:24 -0500

    Day 5 part 1

---
+++ day5/part1.py
@@ -0,0 +1,35 @@
+import queue
+import os
+import re
+
+with open("input.txt") as f:
+    lines = [x.replace("\n", "") for x in f.readlines()]
+
+stacks = re.sub(" +", " ", lines[8]).strip().split(" ")
+stack_index_locations = {x: lines[8].index(x) for x in stacks}
+stack_queues = {x: queue.LifoQueue() for x in stack_index_locations}
+
+# Begin process to create stacks
+for line_num in [x for x in range(7, -1, -1)]:
+	line = lines[line_num]
+	for stack_num, idx in stack_index_locations.items():
+		value_present = line[idx]
+		if value_present != " ":
+			stack_queues[stack_num].put(value_present)
+
+def parse_instruction(s):
+	split_string = s.split(" ")
+	return (int(split_string[1]), split_string[3], split_string[5])
+
+for instruction in lines[10:]:
+	parsed_instruction = parse_instruction(instruction)
+	for move_num in range(1, parsed_instruction[0] + 1):
+		removed_item = stack_queues[parsed_instruction[1]].get()
+		stack_queues[parsed_instruction[2]].put(removed_item)
+
+result = [stack_queues[str(x)].get() for x in range(1,10)]
+
+
+# Notes:
+# 	Input in format of "Move {num_to_move} from {column_num} to {column_num}"
+#	Boxes get pulled top first
