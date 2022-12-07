9195f253ba963175e2bdd16e1509f070d0cd193d
Michael Stratton <mastratton3@gmail.com>
2022-12-07 10:20:41 -0500

    modifications to day3

--- day3/part2.py
+++ day3/part2.py
@@ -3,6 +3,7 @@ import os
 with open("input.txt") as f:
     lines = f.readlines()
 
+
 def char_range(c1, c2):
     """Generates the characters from `c1` to `c2`, inclusive."""
     for c in range(ord(c1), ord(c2)+1):
@@ -16,10 +17,18 @@ letter_to_score.update({x: y for (x, y) in zip([x for x in char_range('A', 'Z')]
 def common_in_three(l1, l2, l3):
     return set(l1).intersection(set(l2)).intersection(set(l3))
 
-a = "vJrwpWtwJgWrhcsFMMfFFhFp"
-b = "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL"
-c = "PmmdzqPrVvPwwTWBwg"
+def score_items(s1):
+    return [letter_to_score[x] for x in s1]
 
-common_in_three(a, b, c)
+def score_and_sum_items(s1):
+    return sum(score_items(s1))
 
-chunks = [lines[x:x+3] for x in range(0, len(a), 3)]
+def split_overlap_and_score(l):
+    l1, l2 = split_list_in_half(l)
+    return score_and_sum_items(find_overlapping_items(l1, l2))
+
+chunks = [lines[x:x+3] for x in range(0, len(lines), 3)]
+common_across = [common_in_three(x[0], x[1], x[2]) for x in chunks]
+scores = [score_and_sum_items(x) for x in common_across]
+
+sum(scores)
